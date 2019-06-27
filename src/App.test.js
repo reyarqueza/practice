d77f8f4bbf0c49ca8ad35d9424bd59cc1df44c37
Rey Arqueza <reyarqueza@gmail.com>
2019-06-27 15:38:10 -0700

    separated unit tests into test suites with 'describe'

--- src/App.test.js
+++ src/App.test.js
@@ -9,41 +9,45 @@ const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`)
 }
 
-test('renders component-app', () => {
-  const wrapper = shallow(<App />)
-  const appComponent = findByTestAttr(wrapper, 'component-app')
-  expect(appComponent.length).toBe(1)
+describe('renders the UI of the page', () => {
+  test('renders component-app', () => {
+    const wrapper = shallow(<App />)
+    const appComponent = findByTestAttr(wrapper, 'component-app')
+    expect(appComponent.length).toBe(1)
+  })
+
+  test('renders increment button', () => {
+    const wrapper = shallow(<App />)
+    const button = findByTestAttr(wrapper, 'increment-button')
+    expect(button.length).toBe(1)
+  })
+
+  test('renders counter display', () => {
+    const wrapper = shallow(<App />)
+    const counterDisplay = findByTestAttr(wrapper, 'counter-display')
+    expect(counterDisplay.length).toBe(1)
+  })
 })
 
-test('renders increment button', () => {
-  const wrapper = shallow(<App />)
-  const button = findByTestAttr(wrapper, 'increment-button')
-  expect(button.length).toBe(1)
-})
+describe('counter related tests', () => {
+  test('counter starts at 0', () => {
+    const wrapper = shallow(<App />)
+    const initialStateCount = wrapper.state('count')
+    expect(initialStateCount).toBe(8)
+  })
 
-test('renders counter display', () => {
-  const wrapper = shallow(<App />)
-  const counterDisplay = findByTestAttr(wrapper, 'counter-display')
-  expect(counterDisplay.length).toBe(1)
+  test('clicking button increments counter display', () => {
+    const wrapper = mount(<App />)
+    const button = findByTestAttr(wrapper, 'increment-button')
+    const counterDisplay = findByTestAttr(wrapper, 'counter-display')
+    const initialCounter = 8
+    wrapper.setState({
+      count: initialCounter
+    })
+    console.log('before counterDisplay.text()', counterDisplay.text())
+    button.simulate('click')
+    wrapper.update()
+    console.log('after counterDisplay.text()', counterDisplay.text())
+    expect(counterDisplay.text()).toContain(initialCounter + 1)
+  })
 })
-
-test('counter starts at 0', () => {
-  const wrapper = shallow(<App />)
-  const initialStateCount = wrapper.state('count')
-  expect(initialStateCount).toBe(8)
-})
-
-// test('clicking button increments counter display', () => {
-//   const wrapper = mount(<App />)
-//   const button = findByTestAttr(wrapper, 'increment-button')
-//   const counterDisplay = findByTestAttr(wrapper, 'counter-display')
-//   const initialCounter = 8
-//   wrapper.setState({
-//     count: initialCounter
-//   })
-//   console.log('before counterDisplay.text()', counterDisplay.text())
-//   button.simulate('click')
-//   wrapper.update()
-//   console.log('after counterDisplay.text()', counterDisplay.text())
-//   expect(counterDisplay.text()).toContain(initialCounter + 1)
-// })
